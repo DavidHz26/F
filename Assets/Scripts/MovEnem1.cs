0c0531c73157cb52b8bc0b51383a6eb50a5571e1
JosZarate <pepe-ds-pokes@hotmail.com>
2018-03-13 23:45:42 -0600

    Agregando enemigos

---
+++ Assets/Scripts/MovEnem1.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovEnem1 : MonoBehaviour
+{
+	float velocidad = 3;
+	float tiempo = 0;
+	float dash = 0;
+	
+	void Update () 
+	{
+		tiempo += Time.deltaTime;
+		dash += Time.deltaTime;
+		
+		if(tiempo < 3)
+		{
+			GetComponent<Rigidbody2D>().velocity = new Vector2(velocidad, GetComponent<Rigidbody2D>().velocity.y);
+		}
+		if(tiempo > 3 && tiempo < 6)
+		{
+			GetComponent<Rigidbody2D>().velocity = new Vector2(-velocidad, GetComponent<Rigidbody2D>().velocity.y);
+		}
+		if(tiempo > 6)
+		{
+			tiempo = 0;
+		}
+		
+		if(dash > 4)
+		{
+			velocidad = 8;
+		}
+		if(dash > 6)
+		{
+			velocidad = 3;
+			dash = 0;
+		}
+	}
+}
---
+++ Assets/Scripts/MovEnem2.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovEnem2 : MonoBehaviour {
+
+	float velocidad = 5;
+	float tiempo = 0;
+	
+	
+	void Update () 
+	{
+		tiempo += Time.deltaTime;
+		
+		if(tiempo < 3)
+		{
+			GetComponent<Rigidbody2D>().velocity = new Vector2(velocidad, GetComponent<Rigidbody2D>().velocity.y);
+		}
+		if(tiempo > 3 && tiempo < 6)
+		{
+			GetComponent<Rigidbody2D>().velocity = new Vector2(-velocidad, GetComponent<Rigidbody2D>().velocity.y);
+		}
+		if(tiempo > 6)
+		{
+			tiempo = 0;
+		}
+	}
+}
