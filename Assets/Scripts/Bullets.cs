4803e61e1a55faba722bf91527a32327f27a4557
JosZarate <pepe-ds-pokes@hotmail.com>
2018-03-15 00:00:33 -0600

    Enemigos02

---
+++ Assets/Scripts/Bullets.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullets : MonoBehaviour {
+
+	float Speed = 0.3f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.position += transform.up * Speed;
+
+		Destroy (gameObject, 0.5f);
+
+	}
+}
--- Assets/Scripts/MovEnem2.cs
+++ Assets/Scripts/MovEnem2.cs
@@ -3,14 +3,29 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class MovEnem2 : MonoBehaviour {
-
+	
+	int Direction = 180;
 	float velocidad = 5;
 	float tiempo = 0;
+	
+	public GameObject bala;
+	GameObject balaActual;
+	
+	float Timer = 0;
+	
+	SpriteRenderer spr;
+	
+	void Start()
+	{
+		spr = GetComponent<SpriteRenderer>();
+		balaActual = bala;
+	}
 	
 	
 	void Update () 
 	{
 		tiempo += Time.deltaTime;
+		Timer += Time.deltaTime;
 		
 		if(tiempo < 3)
 		{
@@ -23,6 +38,12 @@ public class MovEnem2 : MonoBehaviour {
 		if(tiempo > 6)
 		{
 			tiempo = 0;
+		}
+		
+		if(Timer > 1.5)
+		{
+			Instantiate (bala, transform.position, Quaternion.Euler (0, 0, Direction));
+			Timer = 0;
 		}
 	}
 }
