761e625afa9525e64013e20f9364eabcdec2b4d7
JosZarate <pepe-ds-pokes@hotmail.com>
2018-04-04 08:48:36 -0500

    algo

---
+++ Assets/Scripts/patron_jefe.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class patron_jefe : MonoBehaviour 
+{
+	float velocidad = 6;
+	float dash = 0;
+	
+	void Start () 
+	{
+		
+	}
+	
+	
+	void Update () 
+	{
+		
+	}
+}
