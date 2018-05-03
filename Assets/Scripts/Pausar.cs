c952ee648c1bad3701613d73d00674ef1a09d38a
JosZarate <pepe-ds-pokes@hotmail.com>
2018-05-03 16:09:04 -0500

    algo

---
+++ Assets/Scripts/Pausar.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pausar : MonoBehaviour 
+{
+	public GameObject a;
+	
+	void Start () 
+	{
+	}
+	
+	void Update ()
+	{
+		if(Input.GetKeyDown(KeyCode.P))
+		{
+			a.SetActive(true);
+			Time.timeScale = 0;
+		}
+	}
+	
+	public void Continuar()
+	{
+		a.SetActive(false);
+		Time.timeScale = 1;
+	}
+}
