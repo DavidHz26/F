230c6daf2d62372d1f8a5b6010859040d9198f81
David Hdz <dave.hzad@gmail.com>
2018-05-03 15:48:04 -0500

    Menu

---
+++ Assets/Scripts/MenuController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuController : MonoBehaviour {
+
+	//Jugar
+	void Play(){
+		SceneManager.LoadScene("Demo");
+	}
+	
+	void Credits(){
+		SceneManager.LoadScene("Creditos");
+	}
+	
+	void Exit(){
+		Application.Quit;
+	}
+}
