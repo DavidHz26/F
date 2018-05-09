509be2ee48f51b797ac1320a87ac925031cba8c1
David Hdz <dave.hzad@gmail.com>
2018-05-08 19:12:33 -0500

    Salto_y_sandia

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -9,25 +9,21 @@ public class PlayerController : MonoBehaviour {
 	public float velWalk;
 	public float actualWalk;
 
-	public float VelRun;
-	public float actualRun;
-
 	public float Jump;
 
 	public bool ground;
-	public bool running;
 	public bool idle;
 	
 	public float vida = 3;
 
 	SpriteRenderer spr;
 
+
 	public Sprite Default;
 	public Sprite Right;
 	public Sprite Up;
 	public Sprite Down;
 	public Sprite sJump;
-	public Sprite Run;
 	
 	public GameObject SecondCamera;
 	public GameObject CanvasGO;
@@ -35,38 +31,40 @@ public class PlayerController : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
 		spr = GetComponent<SpriteRenderer> ();
+	
 		actualWalk = velWalk;
-		actualRun = VelRun;
 	}
 
 	// Update is called once per frame
 	void Update () {
 
 		idle = true;
-		running = false;
-
+		
+		//Movimiento
+		
 		float xAxis = Input.GetAxis ("Horizontal");
-		float yAxis = Input.GetAxis ("Vertical");
-
-		Vector3 movement = transform.TransformDirection (xAxis, yAxis, 0);
+		
+		Vector3 movement = transform.TransformDirection (xAxis, 0, 0);
 
 		float leftxAxis = Input.GetAxis ("LeftJoystick_X");
-		float leftyAxis = Input.GetAxis ("LeftJoystick_Y");
-
+		
 		transform.Translate (movement * velWalk);
 
 
 		///ANIMACIONES!!!!
 
+
 		//Movimiento Derecha
 		if (xAxis >= 1) {
 			idle = false;
+			spr.sprite = Right;
 			spr.flipX = false;
 		}
 
 		//Movimiento Izquierda
 		else if (xAxis <= -1) {
 			idle = false;
+			spr.sprite = Right;
 			spr.flipX = true;
 		}
 
@@ -80,62 +78,21 @@ public class PlayerController : MonoBehaviour {
 			idle = false;
 		}
 
-		//Saltar
-		if (yAxis >= 1 && ground == true) {
-			idle = false;
-			ground = false;
-			velWalk = Jump;
-		} else {
-			velWalk = actualWalk;
-		}
-
-		if (idle == false) {
-			spr.sprite = Right;
-		}
-
-		if (ground == false) {
-			spr.sprite = sJump;
-		}
-
-		if (running == true) {
-			spr.sprite = Run;
-		}
-
 		//------------------------------------------------------------------
 
-		//Correr Derecha
-		if (xAxis >= 1 && Input.GetButton ("A")) {
+		//Saltar
+		if (Input.GetButton ("A") && ground == true) {
 			idle = false;
-			running = true;
-			velWalk = VelRun;
+			ground = false;
+			transform.Translate(Vector3.up * Jump);
+			spr.sprite = sJump;
+			
 		}
 
-		//Correr Izquierda
-		else if (xAxis <= -1 && Input.GetButton ("A")) {
-			idle = false;
-			running = true;
-			velWalk = VelRun;
-		} else {
-			velWalk = actualWalk;
-		}
-
-		//Mover la direccion de disparo
-		if (leftyAxis <= -1 && ground == true) {
-			idle = false;
-		} else if (leftyAxis <= -1 && ground == false) {
-			idle = false;
-		} else if (leftyAxis >= 1 && ground == true) {
-			idle = false;
-		} else if (leftyAxis >= 1 && ground == false) {
-			idle = false;
-		}
 
 		else {
-			if(ground==true && running == false && idle == true){
+			if(ground==true &&  idle == true){
 				spr.sprite = Default;
-			}
-			else if(ground==true && running == true && idle==false){
-				spr.sprite = Run;
 			}
 		}
 		
@@ -159,13 +116,12 @@ public class PlayerController : MonoBehaviour {
 
 			ground = true;
 
-			if (spr.sprite == sJump && running == false) {
+			if (spr.sprite == sJump) {
 				spr.sprite = Default;
 			} 
-			else if (spr.sprite == sJump && running == true) {
-				spr.sprite = Run;
-			}
 		}
+		
+		//Controlador de vida
 		
 		if(_col.gameObject.tag == "enemigote" || _col.gameObject.tag == "balin" || _col.gameObject.tag == "balon")
 		{
